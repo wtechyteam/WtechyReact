b668b50e250ce6e22398bc71ac54437f90a126ff
HP <you@example.com>
2024-04-05 15:31:54 +0530

    navbar

--- src/components/header/navbar/navbar.jsx
+++ src/components/header/navbar/navbar.jsx
@@ -3,21 +3,32 @@ import { Accordion, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 import Logo from '../../common/SiteLogo';
 import headerLogo from '../../../assets/Images/siteLogo.png'
 import { FiChevronDown } from "react-icons/fi";
-import { Link } from 'react-router-dom';
 import { headerData } from '../../data/headerData';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderNavbar = ({ activeTab, setActiveTab }) => {
+    const navigate = useNavigate();
     const [hover, setHover] = useState(false)
     const [show, setShow] = useState(false);
     const [activeLink, setActiveLink] = useState();
+    const [serviceTab, setServiceTab] = useState(false)
 
-    const handleTab = (item) => {
-        setActiveTab(item.id);
+    const handleTab = (items) => {
+        setActiveTab(items.id);
+        setServiceTab(false);
+        navigate(`${items.link !== '' ? items.link : undefined}`)
+        setShow(false)
     };
-    const handleLink = (item) => {
+    const handleLink = (item, items) => {
         setActiveLink(item.id);
         setHover(false)
-
+        setShow(false)
+        navigate(`${item.path}`)
+        if (item.key === true) {
+            setServiceTab(true)
+        } else {
+            setServiceTab(false)
+        }
     };
 
     const toggleOffCanvas = () => {
@@ -37,96 +48,65 @@ const HeaderNavbar = ({ activeTab, setActiveTab }) => {
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
                             <div className='display_768 d-none d-xl-flex'>
-                                {headerData.map((item, index) => (
+                                {headerData.map((items, index) => (
                                     <div key={index}>
-                                        <div onMouseEnter={item.menuType ? () => setHover(true) : () => setHover(false)} onMouseLeave={() => setHover(false)} className={item.menuType ? "hover_menu" : ""}>
-                                            <Link to={item.link !== '' ? item.link : undefined} className={item.customClass}>
-                                                <span onClick={() => handleTab(item)} className={`title_name ${activeTab === item.id && "active_tab"}`}>
-                                                    {item.title}
-                                                    {item.subMenu && <FiChevronDown className='ms-1' />}
-                                                </span>
-                                            </Link>
+                                        <div onMouseEnter={items.menuType ? () => setHover(true) : () => setHover(false)} onMouseLeave={() => setHover(false)} className={items.menuType ? "hover_menu" : ""}>
+                                            <p onClick={() => handleTab(items)}
+                                                className={items.customClass || `title_name ${serviceTab ? (serviceTab && items.ServiceClass) : (activeTab === items.id && "active_tab")}`}>
+                                                {items.title}
+                                                {items.subMenu && <FiChevronDown className='ms-1' />}
+                                            </p>
                                             {hover && <div className='display'>
-                                                {item.subMenu && item.subMenu.map((ele, index) => (
+                                                {items.subMenu && items.subMenu.map((ele, index) => (
                                                     <div key={index} className='hover_subtitle '>
                                                         {ele.subtitle}
-                                                        {ele.subLinks && ele.subLinks.map((item) => {
-                                                            return (
-                                                                <>
-                                                                    <Link to={item.path} className='text-decoration-none'>
-                                                                        <p onClick={() => handleLink(item)} className={`subtitle_name ${activeLink === item.id && "active_Link"}`}>{item.subLinkName}</p>
-                                                                    </Link>
-                                                                </>
-                                                            )
-                                                        })}
+                                                        {ele.subLinks && ele.subLinks.map((item) => (
+                                                            <p onClick={() => handleLink(item, items)} className={`subtitle_name ${activeLink === item.id && "active_Link"}`}>{item.subLinkName}</p>
+                                                        ))}
                                                     </div>
                                                 ))}
                                             </div>}
-
                                         </div>
-
-
                                     </div>
                                 ))}
-
                             </div>
                             <div className='d-xl-none'>
-                                {headerData && headerData.map((item, index) => {
+                                {headerData && headerData.map((items, index) => {
                                     return (
                                         <div key={index}>
                                             {
-                                                item.subMenu ? (
-                                                    <>
-
-                                                        <Accordion className='drops'>
-                                                            <Accordion.Item eventKey={item.eventKey}>
-                                                                <Accordion.Header>{item.title}</Accordion.Header>
-                                                                {item.subMenu && item.subMenu.map((ele, index) => (
-                                                                    <Accordion.Body key={index}>
-                                                                        <span className='subtitle_name'>
-                                                                            <Accordion className='drops'>
-                                                                                <Accordion.Item eventKey={item.eventKey}>
-                                                                                    <Accordion.Header>{ele.subtitle}</Accordion.Header>
-                                                                                    <Accordion.Body>
-                                                                                        {ele.subLinks && ele.subLinks.map((item, index) => {
-                                                                                            return (
-                                                                                                <>
-                                                                                                    <Link key={index} to={item.path} className='text-decoration-none'>
-                                                                                                        <p onClick={toggleOffCanvas} className='subtitle_name_small'>{item.subLinkName}</p>
-                                                                                                    </Link>
-                                                                                                </>
-                                                                                            )
-                                                                                        })}
-                                                                                    </Accordion.Body>
-                                                                                </Accordion.Item>
-                                                                            </Accordion>
-
-
-                                                                        </span>
-                                                                    </Accordion.Body>
-                                                                ))}
-
-                                                            </Accordion.Item>
-
-                                                        </Accordion>
-                                                    </>
+                                                items.subMenu ? (
+                                                    <Accordion className='drops'>
+                                                        <Accordion.Item eventKey={items.eventKey}>
+                                                            <Accordion.Header>{items.title}</Accordion.Header>
+                                                            {items.subMenu && items.subMenu.map((ele, index) => (
+                                                                <Accordion.Body key={index}>
+                                                                    <Accordion className='drops'>
+                                                                        <Accordion.Item eventKey={items.eventKey}>
+                                                                            <Accordion.Header>{ele.subtitle}</Accordion.Header>
+                                                                            <Accordion.Body>
+                                                                                {ele.subLinks && ele.subLinks.map((item, index) => (
+                                                                                    <div key={index}>
+                                                                                        <p onClick={() => handleLink(item)} className='subtitle_name_small'>{item.subLinkName}</p>
+                                                                                    </div>
+                                                                                ))}
+                                                                            </Accordion.Body>
+                                                                        </Accordion.Item>
+                                                                    </Accordion>
+                                                                </Accordion.Body>
+                                                            ))}
+                                                        </Accordion.Item>
+                                                    </Accordion>
                                                 )
                                                     :
-                                                    <Link className='text-decoration-none' to={item.link !== '' ? item.link : undefined}>
-
-                                                        <div onClick={toggleOffCanvas} className='subtitle_name'>{item.title}</div>
-                                                    </Link>
+                                                    <p onClick={() => handleTab(items)} className='subtitle_name'>{items.title}</p>
                                             }
                                         </div>
                                     )
                                 }
-
-
                                 )}
-
                             </div>
                         </Nav>
-
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
             </Container>
