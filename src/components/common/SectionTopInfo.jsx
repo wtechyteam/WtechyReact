d7bb1cdf624f5ed0a5fa235882de964a1e7cfbe6
HP <you@example.com>
2024-04-06 15:02:49 +0530

    google Ads management

--- src/components/common/SectionTopInfo.jsx
+++ src/components/common/SectionTopInfo.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import ContactUsButton from './ContactUsButton';
 
 
-const SectionTopInfo = ({ smallTitle,title, text,isCenter ,isFullWidth}) => {
+const SectionTopInfo = ({ smallTitle, title, text, isCenter, isFullWidth, showContactBtn, description }) => {
 
   const alignmentClass = isCenter ? 'text-center mx-auto' : '';
 
   return (
-    <div className={`sectionTopInfo  ${isFullWidth == true ? 'col-md-10' : 'col-md-6'} + ${alignmentClass}`} >
+    <div className={`sectionTopInfo  ${isFullWidth === true ? 'col-md-10' : 'col-md-6'} + ${alignmentClass}`} >
       <h4 className='section-title-sm '>{smallTitle}</h4>
       <h2 className='title-xl fw-bold'>{title}</h2>
       <p className=''>{text}</p>
+      <div dangerouslySetInnerHTML={{ __html: description }}></div>
+      {showContactBtn && (
+        <ContactUsButton />
+      )}
     </div>
   );
 };
