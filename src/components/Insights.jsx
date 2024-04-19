a2998886233e183bffdc1703a95eff074f6ed53a
HP <you@example.com>
2024-04-19 15:47:27 +0530

    loader

--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -8,31 +8,34 @@ import { fetchInsights } from '../redux/actions/insightsAction';
 import { useDispatch, useSelector } from 'react-redux';
 import { scrollTop } from './common/utils';
 import { FiClock } from "react-icons/fi";
+import LoadingSection from './common/loadingSection';
 
 
 
 const Insights = () => {
     const dispatch = useDispatch();
-    const { insights } = useSelector((state) => state.insightsApi);
+    const { insights, loading } = useSelector((state) => state.insightsApi);
 
     useEffect(() => {
         dispatch(fetchInsights());
     }, [dispatch]);
 
+    //   ********************loading-card***********************
     const postsPerPage = 6;
     const [displayedPosts, setDisplayedPosts] = useState(postsPerPage);
-    const [loading, setLoading] = useState(false); // State to track loading status
+    const [loadings, setLoadings] = useState(false); // State to track loadings status
 
     const handleLoadMore = () => {
-        setLoading(true); // Set loading to true when button is clicked
+        setLoadings(true); // Set loadings to true when button is clicked
         setTimeout(() => {
             setDisplayedPosts(prevCount => prevCount + postsPerPage);
-            setLoading(false); // Set loading to false after 1 second delay
+            setLoadings(false); // Set loadings to false after 1 second delay
         }, 1000);
     };
+    // *******************************************************
 
-     // Function to format date
-     const formatDate = (dateString) => {
+    // Function to format date
+    const formatDate = (dateString) => {
         const date = new Date(dateString);
         // Extract date components
         const day = date.getDate();
@@ -54,32 +57,39 @@ const Insights = () => {
                         title={'Our Latest Insights'}
                         text={'Our tailored digital marketing strategies are designed to deliver significant results, ensuring your business stands out and achieves success in the digital landscape.'}
                     />
-                    <div className="featured-posts row justify-content-center mt-5">
-                        {insights.slice(0 , displayedPosts)?.map((post, index) => (
-                            <div className="col-lg-4 col-md-6 mb-4" key={post?.id}>
-                                <div className="postCard hasShadow">
-                                    <Link to={post?.slug} className="postImageWrap" onClick={scrollTop}>
-                                        {post?.acf_fields && post?.acf_fields?.FeacherdImage !== false && (
-                                            <img src={post?.acf_fields?.FeacherdImage} alt={post?.title?.rendered} />
-                                        )}
-                                    </Link>
-                                    <div className="post-details">
-                                        <h3 className='post-title title-md fw-bold'>
-                                            <Link to={post?.slug} onClick={scrollTop}>{post?.title?.rendered}</Link>
-                                        </h3>
-                                        <p className='post-date d-flex align-items-center'><FiClock className='me-2'/>{formatDate(post.date)}</p>
-                                        {post?.acf_fields && (
-                                            <p className='post-info'>{post?.acf_fields?.shortDiscription}</p>
-                                        )}
-                                        <Link className='dBtn btnText' to={post?.slug} onClick={scrollTop} >Read More<FiChevronsRight /></Link>
-                                    </div>
+                    {loading ? <LoadingSection /> :
+                        (
+                            <>
+                                <div className="featured-posts row justify-content-center mt-5">
+                                    {insights.slice(0, displayedPosts)?.map((post, index) => (
+                                        <div className="col-lg-4 col-md-6 mb-4" key={post?.id}>
+                                            <div className="postCard hasShadow">
+                                                <Link to={post?.slug} className="postImageWrap" onClick={scrollTop}>
+                                                    {post?.acf_fields && post?.acf_fields?.FeacherdImage !== false && (
+                                                        <img src={post?.acf_fields?.FeacherdImage} alt={post?.title?.rendered} />
+                                                    )}
+                                                </Link>
+                                                <div className="post-details">
+                                                    <h3 className='post-title title-md fw-bold'>
+                                                        <Link to={post?.slug} onClick={scrollTop}>{post?.title?.rendered}</Link>
+                                                    </h3>
+                                                    <p className='post-date d-flex align-items-center'><FiClock className='me-2' />{formatDate(post.date)}</p>
+                                                    {post?.acf_fields && (
+                                                        <p className='post-info'>{post?.acf_fields?.shortDiscription}</p>
+                                                    )}
+                                                    <Link className='dBtn btnText' to={post?.slug} onClick={scrollTop} >Read More<FiChevronsRight /></Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
-                            </div>
-                        ))}
-                    </div>
-                    {displayedPosts < insights.length && (
-                        <button className={`d-block mt-4 dBtn btnBorder mx-auto ${loading && 'disabled'}`} onClick={handleLoadMore}>{loading ? 'Loading...' : 'Load More'}</button>
-                    )}
+                                {displayedPosts < insights.length && (
+                                    <button className={`d-block mt-4 dBtn btnBorder mx-auto ${loadings && 'disabled'}`} onClick={handleLoadMore}>{loadings ? 'Loadings...' : 'Load More'}</button>
+                                )}
+                            </>
+                        )
+                    }
+
                 </Container>
             </section>
         </>
