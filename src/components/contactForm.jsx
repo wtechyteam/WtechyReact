9f5262ad581b430df1782ca93c60ccb08a3ee87a
vijayKumarSonii <you@example.com>
2024-05-23 11:33:45 +0530

    contact-form

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,55 +1,56 @@
-import { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+"use client"
+import { useState } from 'react';
 import PopupModal from './common/modal';
 
 const MyForm = () => {
     const [show, setShow] = useState(false);
+    const [contactData, setContactData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        subject: "",
+        message: ""
+    })
 
+    const handleShowModal = () => setShow(true);
     const handleCloseModal = () => {
         window.location.reload();
         setShow(false);
     }
 
-    const handleShowModal = () => setShow(true);
-
-    const form = useRef();
-
-    const sendEmail = (e) => {
+    const handleSubmit = (e) => {
+        console.log("formData", contactData)
         e.preventDefault();
-        emailjs
-            .sendForm('service_8tiqz08', 'template_ojpyfvk', form.current, {
-                publicKey: 'R3qMEbTFpNoo0gXc8',
-            })
-            .then(
-                () => {
-                    setShow(true)
-                },
-                (error) => {
-                    alert('FAILED...', error.text);
-                },
-            );
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setContactData({
+            ...contactData,
+            [name]: value
+        });
     };
+
     return (
         <>
-            <form className="customForm bg-gray px-4 py-5 rounded-2" ref={form} onSubmit={sendEmail}>
+            <form className="customForm bg-gray px-4 py-5 rounded-2" onSubmit={handleSubmit}>
                 <div className="row mx-0">
                     <h3 className='title-lg fw-bold'>How Can We Help?</h3>
                     <p>Your email address will not be published. Required fields are marked *</p>
                     <div className='inputWrap col-lg-6'>
-                        <input placeholder='Name*' className='form_input' type="text" name="from_name" required />
+                        <input onChange={handleInputChange} placeholder='Name*' className='form_input' value={contactData.name} type="text" name="name" required />
                     </div>
                     <div className='inputWrap col-lg-6'>
-                        <input className='form_input' placeholder='Email*' type="email" name="from_email" required />
+                        <input onChange={handleInputChange} className='form_input' placeholder='Email*' value={contactData.email} type="email" name="email" required />
                     </div>
                     <div className='inputWrap col-lg-6'>
-                        <input placeholder='Number*' className='form_input' type="number" name="phone" required />
+                        <input onChange={handleInputChange} placeholder='Number*' className='form_input' value={contactData.phone} type="number" name="phone" required />
                     </div>
                     <div className='inputWrap col-lg-6'>
-                        <input className='form_input' type="text" name="subject" placeholder="How can we help you?*" required />
+                        <input onChange={handleInputChange} className='form_input' value={contactData.subject} type="text" name="subject" placeholder="How can we help you?*" required />
                     </div>
-
                     <div className='inputWrap col-lg-12'>
-                        <textarea className='form_input' placeholder='Message...' name="message" />
+                        <textarea onChange={handleInputChange} className='form_input' placeholder='Message...' value={contactData.message} name="message" />
                     </div>
                     <div className="col-12">
                         <button className='dBtn btnSecondary rounded-0 hasShadow w-100 py-2' type="submit" >Contact Us</button>
@@ -58,7 +59,6 @@ const MyForm = () => {
             </form>
             <PopupModal handleCloseModal={handleCloseModal} handleShowModal={handleShowModal} show={show} />
         </>
-
     )
 }
 export default MyForm;
