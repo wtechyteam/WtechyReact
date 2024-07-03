4aaeaebc2c5ac7000d924df27d0048dd0bce73d1
john <john.flagan@gmail.com>
2024-07-03 18:29:39 +0530

    Your commit message

--- deploy.sh
+++ deploy.sh
@@ -27,14 +27,16 @@ git add . || { echo "Failed to add changes to git"; exit 1; }
 echo "Committing changes to git..."
 git commit -m "$COMMIT_MESSAGE" || { echo "Failed to comit changes to git"; exit 1; }
 
+
 # Push changes to git
 echo "Pushing changes to git..."
 git push || { echo "Failed to push changes to git"; exit 1; }
 
 # Connect to the hosting server and pull the latest code
 echo "Connecting to the hosting server and pulling the latest code..."
-ssh $HOSTING_SERVER_USER@$HOSTING_SERVER << EOF
-  cd $HOSTING_SERVER_REPO_PATH || { echo "Failed to change directory to $HOSTING_SERVER_REPO_PATH"; exit 1; }
+ssh "$HOSTING_SERVER_USER@$HOSTING_SERVER"
+  cd 
+  "$HOSTING_SERVER_REPO_PATH" || { echo "Failed to change directory to"; exit 1; }
   git pull || { echo "Failed to pull changes from git"; exit 1; }
   echo "Code updated on the hosting server."
 EOF
